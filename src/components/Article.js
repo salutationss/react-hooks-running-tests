000190451a8c2ae9b7560fc3696b09f6f1272f27
Alex B <96922302+salutationss@users.noreply.github.com>
2022-06-26 00:56:30 -0400

    commit tree c8029efd3eb3adbca3f1fd2ed27d3671bc241ea8

--- src/components/Article.js
+++ src/components/Article.js
@@ -1,11 +1,10 @@
 import React from "react";
 
 function Article() {
-  return (
-    <div>
-      {/* please pass this test */}
-    </div>
-  );
+    return ( <
+        div > { please pass this test } <
+        /div>
+    );
 }
 
 export default Article;
